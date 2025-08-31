388e3e98fcceb6253127f02e29dbd9b3b5ca4ec7
Domingos de Eulária Dumba <domingosdeeularia@domingosdeeulariadumba.dominosdeeulariadumba>
2025-08-31 09:47:50 +0100

    updated README

--- utils/ai.py
+++ utils/ai.py
@@ -53,9 +53,7 @@ def ask_xplendid(session_state, lang = 'en'):
         is_two_tailed_xp, n_ctrl_xp, p_ctrl_xp, n_trmt_xp, p_trmt_xp, 
         results_and_recommendation, min_sample_size
         )
-    
-    print(prompt) # Prompt Debugging
-    
+   
     # Looping through all API keys
     messages = session_state.chat_history 
     conversation = [{'role': 'system', 'content': prompt}] + messages 
