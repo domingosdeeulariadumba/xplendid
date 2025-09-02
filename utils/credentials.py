96bbca7b0e95c8ca2b49b85135bf5ebd0b6dec0f
domingosdeeulariadumba <domingosdeeularia@domingosdeeulariadumba.dominosdeeulariadumba>
2025-09-02 16:14:48 +0100

    condensed load_credentials

--- utils/credentials.py
+++ utils/credentials.py
@@ -2,7 +2,7 @@
 import streamlit as st
 
 def load_credentials(): 
-    api_keys = st.secrets['api']['keys']
-    base_urls = st.secrets['api']['base_urls']
-    models = st.secrets['api']['models']
+    api_keys, base_urls, models = tuple(
+        [st.secrets['api'][i] for i in ['keys','base_urls''models']]
+    )
     return zip(api_keys, base_urls, models)
