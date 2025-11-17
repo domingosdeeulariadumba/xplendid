68d339239fe08d3aeb5029e954d81ed5b6cee579
domingosdeeulariadumba <domingosdeeularia@domingosdeeulariadumba.dominosdeeulariadumba>
2025-11-17 13:45:07 +0100

    correct chat availability error

--- ai/assistant.py
+++ ai/assistant.py
@@ -81,6 +81,6 @@ def ask_xplendid(session_state, lang = 'en'):
                 return ai_exception[0]
         except APIConnectionError:
                continue
-        except Exception as e:
-             return e       
+        except:
+             return ai_exception[-1]        
     return ai_exception[1]
